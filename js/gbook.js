c837a36e1814bd11ea6ac79e33e67467bc6c6eaf
kongxu633 <kongxu633@gmail.com>
2016-03-20 10:35:21 +0800

    addcheck

--- js/gbook.js
+++ js/gbook.js
@@ -35,56 +35,81 @@ function mysubmit(){
 }
 
 
+$(document).ready(function() {
 
-//document.querySelector('.weui_uploader_input').addEventListener('change', function () {
-$(".weui_uploader_input").on('change',function(){
-    //正在处理图片
-    showLoadingToast();
+    $("#con").on('keyup',function(){
+        var maxlen = 500;
+        var len = $(this).val().length;
+        if(len >= maxlen){
+            $(this).val($(this).val().substring(0,maxlen));
+            $(".weui_textarea_counter span").text(maxlen);
+        }else {
+            $(".weui_textarea_counter span").text(len);
+        }
+    });
+
+    $(".weui_uploader_input").on('change', function () {
+
+        //正在处理图片
+        showLoadingToast();
 
 
-    lrz(this.files[0])
-        .then(function (result) {
+        lrz(this.files[0])
+            .then(function (result) {
 
-            var clearBase64 = result.base64.substr(result.base64.indexOf(',') + 1);
-            $.ajax({
+                var clearBase64 = result.base64.substr(result.base64.indexOf(',') + 1);
+                $.ajax({
 
-                type: "POST",
-                url: "upload.php",
-                data: { base64_string:clearBase64 },
-                dataType:"json",
-                success: function(data){
-                    if (0 == data.status) {
-                        alert(data.content);
-                        return false;
-                    }else{
+                    type: "POST",
+                    url: "upload.php",
+                    data: {base64_string: clearBase64},
+                    dataType: "json",
+                    success: function (data) {
+                        if (0 == data.status) {
+                            alert(data.content);
+                            return false;
+                        } else {
 
-                        var picstr = '<p><a href="../'+data.url+'" target="_blank">'+data.url+'</a><img src="../'+data.url+'" width="200"></p>';
-                        var nowstr = $('#pics').val();
-                        $('#pics').val(nowstr + picstr);
+                            var picstr = '<p><a href="../' + data.url + '" target="_blank">' + data.url + '</a><img src="../' + data.url + '" width="200"></p>';
+                            var nowstr = $('#pics').val();
+                            $('#pics').val(nowstr + picstr);
 
-                        var attstr= '<li class="weui_uploader_file" style="background-image:url('+data.url+')"></li>';
-                        $(".weui_uploader_files").append(attstr);
-                        return false;
+                            var attstr = '<li class="weui_uploader_file" style="background-image:url(' + data.url + ')"></li>';
+                            $(".weui_uploader_files").append(attstr);
+                            return false;
+                        }
+                    },
+                    beforeSend: function () {
+                        //准备上传
+                    },
+                    complete: function (XMLHttpRequest, textStatus) {
+                        //当请求完成之后调用这个函数，无论成功或失败。传入 XMLHttpRequest 对象，以及一个包含成功或错误代码的字符串。
+                        showToast();
+
+                        //检查是否5个图片了
+                        maxnum = 5;
+                        picnum = $(".weui_uploader_file").length;
+                        if(picnum >= maxnum){
+                            $(".weui_uploader_input_wrp").hide();
+                            $(".weui_cell_ft span").text(maxnum);
+                        }else{
+                            $(".weui_cell_ft span").text(picnum);
+                        }
+                    },
+                    error: function (XMLHttpRequest, textStatus, errorThrown) { //上传失败
+                        //alert(XMLHttpRequest.status);
+                        //alert(XMLHttpRequest.readyState);
+                        alert(textStatus);
                     }
-                },
-                beforeSend: function () {
-                    //准备上传
-                },
-                complete :function(XMLHttpRequest, textStatus){
-                    showToast();
-                },
-                error:function(XMLHttpRequest, textStatus, errorThrown){ //上传失败
-                    //alert(XMLHttpRequest.status);
-                    //alert(XMLHttpRequest.readyState);
-                    alert(textStatus);
-                }
-            });
+                });
 
-        })
-        .catch(function (err) {
-            // 处理失败会执行
-        })
-        .always(function () {
-            // 不管是成功失败，都会执行
-        });
+            });
+            .catch(function (err) {
+                // 处理失败会执行
+            });
+            .always(function () {
+                // 不管是成功失败，都会执行
+            });
+    });
+
 });
